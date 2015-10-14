a28c552128e22ee77b98a4bf018892a4fbe31528
mckinnonr <mckinnon_ross@hotmail.com>
2015-10-14 12:04:03 -0700

    after gulp autoprefixer update

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@
 var gulp = require('gulp'); // Load Gulp!
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
     .pipe(gulp.dest('./css'));
 });
 

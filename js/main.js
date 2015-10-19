6d6336cc08266efc1db19fc67304213d4a0d9556
mckinnonr <mckinnon_ross@hotmail.com>
2015-10-19 12:08:19 -0700

    angluar modified

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@
     $scope.loading = false;
 
     $scope.getPhotos = function (e){
+      e && e.preventDefault();
       $scope.loading=true;
-      e.preventDefault();
+
       var searchInput = "https://api.instagram.com/v1/tags/" + $scope.hashTagName + "/media/recent?callback=JSON_CALLBACK&client_id=8ba7e057fa6c492984c0dd157ae41a51";
       $http.jsonp(searchInput).then(function(response){
         $scope.posts=response.data.data;
         $scope.loading=false;
+        // console.log(response);
       });
-    }
-
-
+    };
   }]);
 }) ();
